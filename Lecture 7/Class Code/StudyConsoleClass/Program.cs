1a465452fcfedb72bf94013795a1ea2f5f05a976
davidbabayan <david.babayan@hotmail.com>
2020-05-12 05:53:48 +0400

    Lecture 7 Exercises Added

---
+++ Lecture 7/Class Code/StudyConsoleClass/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace StudyConsoleClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            OtherConsoleFuncs();
+            Welcoming();
+            Console.ResetColor();
+        }
+
+        static void WorkingWithString()
+        {
+            Console.Write("\tThis text will stay in the same line!");
+            Console.WriteLine("This text will pass to the next line!");
+            Console.WriteLine("Hello World!");
+        }
+
+        static void Table()
+        {
+            Console.WriteLine("ID\tName\tType");
+        }
+
+        static void Reading()
+        {
+            Console.ReadLine();
+        }
+
+        static void Welcoming()
+        {
+            Console.Write("What is your name? ");
+            Console.WriteLine($"Welcome to my code {Console.ReadLine()}!");
+            Console.Write("What is your age? ");
+            Console.WriteLine($"You are {Console.ReadLine()} years old!");
+        }
+
+        static void OtherConsoleFuncs()
+        {
+            Console.BackgroundColor = ConsoleColor.DarkBlue;
+            Console.Beep();
+            Console.Clear();
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.Title = "Experiement Title";
+        }
+    }
+}
---
+++ Lecture 7/Exercises/WelcomeUser/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace WelcomeUser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Graphic.WelcomeMessage();
+            //Մաս 1․ Տպիր "Enter Your name: " և մնա նույն տողում
+            //Կոդի սկիզբ
+
+
+            //Կոդի ավարտ
+            
+            User user = new User (
+                //Մաս 2․ Գրիր օգտվողից մուտք պահանջող կոդ
+                //Կոդի սկիզբ
+
+                
+                //Կոդի ավարտ
+            );
+
+            //Մաս 3․ Տպիր "Enter Your age: " և մնա նույն տողում
+            //Կոդի սկիզբ
+
+
+            //Կոդի ավարտ
+
+            user.SetAge (
+                Int32.Parse (
+                    //Մաս 4․ Գրիր օգտվողից մուտք պահանջող կոդ
+                    //Կոդի սկիզբ
+
+                    
+                    //Կոդի ավարտ
+                )                
+            );
+
+            Console.CursorSize = 25;
+
+            Console.WriteLine(user.PrintState());
+
+            Graphic.EndingMessage();
+        }
+    }
+}
